1fcb39ec55372834a5f48d698e679ebd4e7bc5cb
Joren De Groof <info@jorendegroof.be>
2010-03-24 14:15:16 +0100

    use railtie

---
+++ lib/facebooker/railtie.rb
@@ -0,0 +1,36 @@
+module Facebooker
+  class Railtie < Rails::Railtie
+    railtie_name 'facebooker'
+    
+    require 'facebooker'
+    
+    initializer 'facebooker.require_rails_libraries' do |app|
+      require 'net/http_multipart_post'
+      if defined? Rails
+        require 'facebooker/rails/backwards_compatible_param_checks'
+        require 'facebooker/rails/controller'
+        require 'facebooker/rails/facebook_url_rewriting'
+        require 'facebooker/rails/facebook_session_handling' if Rails.version < '2.3'
+        require 'facebooker/rails/facebook_request_fix' if Rails.version < '2.3'
+        require 'facebooker/rails/facebook_request_fix_2-3' if Rails.version >= '2.3'
+        require 'facebooker/rails/routing'
+        require 'facebooker/rails/facebook_pretty_errors' rescue nil
+        require 'facebooker/rails/facebook_url_helper'
+        require 'facebooker/rails/extensions/rack_setup' if Rails.version > '2.3'
+        require 'facebooker/rails/extensions/action_controller'
+        #require 'facebooker/rails/extensions/action_view'
+        require 'facebooker/rails/extensions/routing'
+      end
+    end
+    
+    initializer 'facebooker.load_confirguration' do |app|
+      facebook_config = "#{RAILS_ROOT}/config/facebooker.yml"
+      FACEBOOKER = Facebooker.load_configuration(facebook_config)
+    end
+    
+    initializer 'facebooker.set_logger' do |app|
+      # enable logger before including everything else, in case we ever want to log initialization
+      Facebooker.logger = RAILS_DEFAULT_LOGGER if Object.const_defined? :RAILS_DEFAULT_LOGGER
+    end
+  end
+end
