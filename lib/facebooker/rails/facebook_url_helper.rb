e75c0863dde97fdc5681cbf6b3041af1a8ae84c8
Jan De Poorter <jan@defv.be>
2010-04-09 14:28:13 +0200

    Remove unused function and indent file correctly

--- lib/facebooker/rails/facebook_url_helper.rb
+++ lib/facebooker/rails/facebook_url_helper.rb
@@ -51,7 +51,7 @@ module ActionView
       # Alters one and only one line of the Rails button_to.  See below.
       def button_to_with_facebooker(name, options={}, html_options = {})
         if !respond_to?(:request_comes_from_facebook?) || !request_comes_from_facebook?
-           button_to_without_facebooker(name,options,html_options)
+          button_to_without_facebooker(name,options,html_options)
         else
           html_options = html_options.stringify_keys
           convert_boolean_attributes!(html_options, %w( disabled ))
@@ -62,12 +62,12 @@ module ActionView
           end
 
           form_method = method.to_s == 'get' ? 'get' : 'post'
-        
+
           request_token_tag = ''
           if form_method == 'post' && protect_against_forgery?
             request_token_tag = tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => form_authenticity_token)
           end
-        
+
           if confirm = html_options.delete("confirm")
             # this line is the only change => html_options["onclick"] = "return #{confirm_javascript_function(confirm)}"
             html_options["onclick"] = "#{confirm_javascript_function(confirm, 'a.getForm().submit();')}return false;"
@@ -75,11 +75,11 @@ module ActionView
 
           url = options.is_a?(String) ? options : self.url_for(options)
           name ||= url
- 
+
           html_options.merge!("type" => "submit", "value" => name)
 
           "<form method=\"#{form_method}\" action=\"#{escape_once url}\" class=\"button-to\"><div>" +
-            method_tag + tag("input", html_options) + request_token_tag + "</div></form>"
+          method_tag + tag("input", html_options) + request_token_tag + "</div></form>"
         end
       end
 
@@ -110,80 +110,47 @@ module ActionView
         #         alias_method_chain :convert_options_to_data_attributes, :facebooker
 
 
-	# Overrides a private method that link_to calls via convert_options_to_javascript! and
-	# also, button_to calls directly.  For Facebook, confirm can be a hash of options to 
-	# stylize the Facebook dialog.  Takes :title, :content, :style options.  See
-	# the Facebook page http://wiki.developers.facebook.com/index.php/FBJS for valid
-	# style formats like "color: 'black', background: 'white'" or like "'color','black'".
-	#
-	# == Examples ==
-	#
-	# link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>"Go to Facebooker?")
-	# link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>{:title=>"the page says:, :content=>"Go to Facebooker?"})
-	# link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>{:title=>"the page says:, :content=>"Go to Facebooker?", :color=>"pink"})
-  def add_confirm_to_attributes_with_facebooker!(confirm, fun = nil)
-    if !respond_to?(:request_comes_from_facebook?) || !request_comes_from_facebook?
-      confirm_javascript_function_without_facebooker(confirm)
-    else
-      if(confirm.is_a?(Hash))
-        confirm_options = confirm.stringify_keys
-		    title = confirm_options.delete("title") || "Please Confirm"
-		    content = confirm_options.delete("content") || "Are you sure?"
-		    button_confirm = confirm_options.delete("button_confirm") || "Okay"
-		    button_cancel = confirm_options.delete("button_cancel") || "Cancel"
-		    style = confirm_options.empty? ? "" : convert_options_to_css(confirm_options)
-	    else
-	      title,content,style,button_confirm,button_cancel = 'Please Confirm', confirm, "", "Okay", "Cancel"
-	    end
-      "var dlg = new Dialog().showChoice('#{escape_javascript(title.to_s)}','#{escape_javascript(content.to_s)}','#{escape_javascript(button_confirm.to_s)}','#{escape_javascript(button_cancel.to_s)}').setStyle(#{style});"+
-	    "var a=this;dlg.onconfirm = function() { #{fun ? fun : 'document.setLocation(a.getHref());'} };"
-	  end
-  end
-
-	alias_method_chain :add_confirm_to_attributes!, :facebooker
-
-	def convert_options_to_css(options)
-	  key_pair = options.shift
-	  style = "{#{key_pair[0]}: '#{key_pair[1]}'"
-	  for key in options.keys
-	    style << ", #{key}: '#{options[key]}'"
-	  end
-	  style << "}"
-	end
-
-	# Dynamically creates a form for link_to with method.  Calls confirm_javascript_function if and 
-	# only if (confirm && method) for link_to
-        def add_method_to_attributes_with_facebooker!(method, url = '', href = nil, confirm = nil)
-          if !respond_to?(:request_comes_from_facebook?) || !request_comes_from_facebook?
-            method_javascript_function_without_facebooker(method,url,href)
- 	  else
-            action = (href && url.size > 0) ? "'#{url}'" : 'a.getHref()'
-            submit_function =
-              "var f = document.createElement('form'); f.setStyle('display','none'); " +
-              "a.getParentNode().appendChild(f); f.setMethod('POST'); f.setAction(#{action});"
-
-            unless method == :post
-              submit_function << "var m = document.createElement('input'); m.setType('hidden'); "
-              submit_function << "m.setName('_method'); m.setValue('#{method}'); f.appendChild(m);"
-            end
-
-            if protect_against_forgery?
-              submit_function << "var s = document.createElement('input'); s.setType('hidden'); "
-              submit_function << "s.setName('#{request_forgery_protection_token}'); s.setValue('#{escape_javascript form_authenticity_token}'); f.appendChild(s);"
-            end
-            submit_function << "f.submit();"
-
-	    if(confirm)
-	      confirm_javascript_function(confirm, submit_function)
-  	    else
-	      "var a=this;" + submit_function
- 	    end
+        # Overrides a private method that link_to calls via convert_options_to_javascript! and
+        # also, button_to calls directly.  For Facebook, confirm can be a hash of options to 
+        # stylize the Facebook dialog.  Takes :title, :content, :style options.  See
+        # the Facebook page http://wiki.developers.facebook.com/index.php/FBJS for valid
+        # style formats like "color: 'black', background: 'white'" or like "'color','black'".
+        #
+        # == Examples ==
+        #
+        # link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>"Go to Facebooker?")
+        # link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>{:title=>"the page says:, :content=>"Go to Facebooker?"})
+        # link_to("Facebooker", "http://rubyforge.org/projects/facebooker", :confirm=>{:title=>"the page says:, :content=>"Go to Facebooker?", :color=>"pink"})
+        def add_confirm_to_attributes_with_facebooker!(html_options, confirm, fun = nil)
+        if !respond_to?(:request_comes_from_facebook?) || !request_comes_from_facebook?
+          add_confirm_to_attributes_without_facebooker!(html_options, confirm)
+        else
+          if(confirm.is_a?(Hash))
+            confirm_options = confirm.stringify_keys
+            title = confirm_options.delete("title") || "Please Confirm"
+            content = confirm_options.delete("content") || "Are you sure?"
+            button_confirm = confirm_options.delete("button_confirm") || "Okay"
+            button_cancel = confirm_options.delete("button_cancel") || "Cancel"
+            style = confirm_options.empty? ? "" : convert_options_to_css(confirm_options)
+          else
+            title,content,style,button_confirm,button_cancel = 'Please Confirm', confirm, "", "Okay", "Cancel"
           end
-	end
+          "var dlg = new Dialog().showChoice('#{escape_javascript(title.to_s)}','#{escape_javascript(content.to_s)}','#{escape_javascript(button_confirm.to_s)}','#{escape_javascript(button_cancel.to_s)}').setStyle(#{style});"+
+          "var a=this;dlg.onconfirm = function() { #{fun ? fun : 'document.setLocation(a.getHref());'} };"
+        end
+      end
 
-	alias_method_chain :add_method_to_attributes!, :facebooker
+      alias_method_chain :add_confirm_to_attributes!, :facebooker
 
+      def convert_options_to_css(options)
+        key_pair = options.shift
+        style = "{#{key_pair[0]}: '#{key_pair[1]}'"
+        for key in options.keys
+          style << ", #{key}: '#{options[key]}'"
+        end
+        style << "}"
+      end
+      
     end
   end
 end
-
